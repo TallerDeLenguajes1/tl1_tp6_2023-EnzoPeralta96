12ab51e782bbcc7889fb053ba809cb1a92e01411
Peralta Enzo <enzash.k@gmail.com>
2023-05-30 00:31:18 -0300

    solcion ejercicio1

---
+++ practico6/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        int a, b;
+        a=10;
+        b=a;
+        Console.WriteLine("valor de a:"+a);
+        Console.WriteLine("valor de b:"+b);
+
+    }
+}
